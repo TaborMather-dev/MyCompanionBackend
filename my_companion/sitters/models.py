ac5c484ea40c8753a8e8fb8495775a570a65ddc0
Tabor <86973131+TaborMather-dev@users.noreply.github.com>
2021-09-13 09:46:21 -0700

    added sitters, clients, and pets

---
+++ my_companion/sitters/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Sitter(models.Model):
+    name = models.CharField(max_length=100, blank=False, default='')
+    phone_number = models.CharField(max_length=10, blank=False, default='')
+    email = models.CharField(max_length=100, blank=False, default='')
+    street_address = models.CharField(max_length=50, blank=False, default='')
+    city = models.CharField(max_length=50, blank=False, default='')
+    state = models.CharField(max_length=50, blank=False, default='')
+    zip_code = models.IntegerField()
+
+    def __str__(self):
+        return self.name
